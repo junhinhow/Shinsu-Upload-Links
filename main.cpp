1e362306c55af3c40cdf0241a13056397b418b27
junhinhow <aaltairjr@hotmail.com>
2024-10-20 21:38:52 -0400

    comments

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 
-// Ver: 1.0.1
+// Ver: 1.0.2
 
 #include <iostream>
 #include <fstream>
@@ -20,224 +20,254 @@ vector<string> thumbs;
 vector<vector<string>> servidores;
 string qualidade; // Uma única qualidade para todos os episódios
 
-// Funço auxiliar para remover espaços em branco no início e no final da string
+/**
+ * Função auxiliar para remover espaços em branco no início e no final da string
+ *
+ * @param str string a ser trimada
+ * @return string sem espaços no início e no final
+ */
 string trim(const string& str) {
+    // Remove espaços no início e no final
     size_t first = str.find_first_not_of(" \t");
+
+    // Se o início for o final, retorna uma string vazia
     if (first == string::npos) return "";
+
+    // Remove espaços no início
     size_t last = str.find_last_not_of(" \t");
+
+    // Se o final for o início, retorna a string sem espaços
     return str.substr(first, (last - first + 1));
 }
 
-// Função para tratar os links das thumbs
-string tratarLinkThumbs(string link) {
-    link.erase(remove(link.begin(), link.end(), ' '), link.end());
 
-    if (link.substr(0, 7) != "http://" && link.substr(0, 8) != "https://") {
-        cout << "Link invlido: " << link << " (certifique-se de que começa com http:// ou https://)" << endl;
+/**
+ * Função para tratar os links das thumbs.
+ * 
+ * Essa função verifica se o Link está no formato correto e o ajusta
+ * para o formato correto, se necessário.
+ * 
+ * @param Link string com o link a ser tratado
+ * @return string com o link tratado
+ */
+string tratarLinkThumbs(string Link) {
+    
+    // Remove espaços no início e no final do Link
+    Link.erase(remove(Link.begin(), Link.end(), ' '), Link.end());
+
+    // Verifica se o Link começa com http:// ou https://
+    if (Link.substr(0, 7) != "http://" && Link.substr(0, 8) != "https://") {
+        // Se o Link não começa com http:// ou https://, exibe uma mensagem de erro
+        cout << "Link invlido: " << Link << " (certifique-se de que começa com http:// ou https://)" << endl;
         return "";
     }
 
-    // Verificação do formato do link
-    if (link.find("/original/") == string::npos) {
-        if (link.find("/w227_and_h127_bestv2/") != string::npos) {
-            link.replace(link.find("/w227_and_h127_bestv2/"), 24, "/original/");
+    // Verificação do formato do Link
+    if (Link.find("/original/") == string::npos) {
+        // Se o Link não contiver /original/, tenta ajustar
+        if (Link.find("/w227_and_h127_bestv2/") != string::npos) {
+            // Se o Link contiver /w227_and_h127_bestv2/, substitui por /original/
+            Link.replace(Link.find("/w227_and_h127_bestv2/"), 24, "/original/");
         }
     }
 
-    // Adiciona .jpg apenas se for um link válido
-    if (link.size() >= 4 && link.substr(link.size() - 4) != ".jpg") {
-        link += ".jpg";
+    // Adiciona .jpg apenas se for um Link válido
+    if (Link.size() >= 4 && Link.substr(Link.size() - 4) != ".jpg") {
+        Link += ".jpg";
     }
 
-    return link;
+    return Link;
 }
 
-// Função para remover hífens e colchetes de qualidade do link
-string removerHifenEColchetes(string link) {
-    link.erase(remove(link.begin(), link.end(), '-'), link.end());
-
-    size_t startPos = link.find("[");
-    while (startPos != string::npos) {
-        size_t endPos = link.find("]", startPos);
-        if (endPos != string::npos) {
-            link.erase(startPos, endPos - startPos + 1);
-        } else {
-            link.erase(startPos);
-            break;
-        }
-        startPos = link.find("[", startPos);
-    }
-
-    link.erase(remove(link.begin(), link.end(), '['), link.end());
-    link.erase(remove(link.begin(), link.end(), ']'), link.end());
-
-    const string qualidades[] = {"SD", "HD", "FHD"};
-    for (const auto& qualidade : qualidades) {
-        size_t pos;
-        while ((pos = link.find(qualidade)) != string::npos) {
-            link.erase(pos, qualidade.length());
-        }
-    }
-
-    link.erase(remove_if(link.begin(), link.end(), ::isspace), link.end());
-
-    return link;
-}
-
-// Função auxiliar para extrair o código único de um link
-string extractCode(const string& link, const string& identifier) {
-    size_t posStart = link.find(identifier);
+/**
+ * Função auxiliar para extrair o código único de um Link.
+ * 
+ * Essa função procura por um identificador (geralmente um nome de domínio) no Link
+ * e extrai o código único que vem logo após o identificador. Por exemplo, se o
+ * Link for "https://example.com/abc123/def456", e o identificador for "example.com/",
+ * a função irá retornar "abc123".
+ * 
+ * @param Link string com o link a ser processado
+ * @param identificador string com o identificador a ser procurado
+ * @return string com o código único extraído do link
+ */
+string extractCode(const string& Link, const string& identificador) {
+    size_t posStart = Link.find(identificador);
     if (posStart == string::npos) return ""; // Identificador não encontrado
 
-    posStart += identifier.length();
-    size_t posEnd = link.find("/", posStart); // Procurar pelo próximo "/"
+    posStart += identificador.length();
+    size_t posEnd = Link.find("/", posStart); // Procurar pelo próximo "/"
 
     // Se não houver outro "/", considerar o final da string
-    return (posEnd == string::npos) ? link.substr(posStart) : link.substr(posStart, posEnd - posStart);
+    return (posEnd == string::npos) ? Link.substr(posStart) : Link.substr(posStart, posEnd - posStart);
 }
 
-// Função para tratar os links dos servidores
-string tratarLinkServidor(string link) {
-    // Remove espaços no início e no final do link
-    link = trim(link);
+/**
+ * Função para tratar os links dos servidores
+ *
+ * Essa função trata os links dos servidores para que estejam no formato correto
+ * para serem usados na geração do arquivo "lista.txt". Ela remove espaços no
+ * início e no final do Link, e extrai o código único da URL. Além disso, ela
+ * remove "embed" do início do código único, se presente, e forma o Link no
+ * formato final desejado para o arquivo "lista.txt".
+ *
+ * @param Link string com o link a ser processado
+ * @return string com o link tratado
+ */
+string tratarLinkServidor(string Link) {
+    // Remove espaços no início e no final do Link
+    Link = trim(Link);
 
-    // Remove hífens e colchetes
-    link = removerHifenEColchetes(link);
+    // Define uma string vazia para o Código Unico
+    string CodigoUnico;
 
-    string uniqueCode;
+    // Formata Links mp4upload.com (/embed- ou /)
+    if (Link.find("mp4upload.com") != string::npos) {
 
-    // Verificar qual servidor está sendo utilizado pelo link
-    if (link.find("mp4upload.com") != string::npos) {
-        uniqueCode = extractCode(link, "embed-");
-        if (uniqueCode.empty()) {
-            uniqueCode = extractCode(link, "mp4upload.com/");
+        // Tenta capturar o código único = CodigoUnico
+        CodigoUnico = extractCode(Link, "embed-");
+
+        // Verificar se o Link foi capturado corretamente
+        if (CodigoUnico.empty()) {
+            CodigoUnico = extractCode(Link, "mp4upload.com/");
         }
 
-        // Remove "embed" do início do uniqueCode, se presente
-        if (uniqueCode.rfind("embed", 0) == 0) { // Verifica se "embed" está no início
-            uniqueCode.erase(0, 5); // Remove os 5 caracteres da palavra "embed"
+        // Remove "embed" do início do CodigoUnico, se presente
+        if (CodigoUnico.rfind("embed", 0) == 0) { // Verifica se "embed" está no início
+            CodigoUnico.erase(0, 5); // Remove os 5 caracteres da palavra "embed"
         }
 
-        if (!uniqueCode.empty()) {
-            link = "https://www.mp4upload.com/embed-" + uniqueCode + ".html"; // O link final agora deve estar correto
+        // Link Final Formatado
+        if (!CodigoUnico.empty()) {
+            Link = "https://www.mp4upload.com/embed-" + CodigoUnico + ".html"; // O Link final agora deve estar correto
         }
 
         // Verificar se há múltiplos ".html" no final e remover os excedentes
-        while (link.size() >= 10 && link.substr(link.size() - 10) == ".html.html") {
-            link.erase(link.size() - 5); // Remove o último ".html"
+        while (Link.size() >= 10 && Link.substr(Link.size() - 10) == ".html.html") {
+            Link.erase(Link.size() - 5); // Remove o último ".html"
         }
-    } else if (link.find("mixdrop.is") != string::npos || link.find("mixdrop.ps") != string::npos) {
+
+    // Formata Links mixdrop.is ou mixdrop.ps (/e/ ou /f/)
+    } else if (Link.find("mixdrop.is") != string::npos || Link.find("mixdrop.ps") != string::npos) {
             // Tenta extrair o código de qualquer uma das variantes
-            uniqueCode = extractCode(link, "mixdrop.is/e/");
-            if (uniqueCode.empty()) {
-                uniqueCode = extractCode(link, "mixdrop.is/f/");
+            CodigoUnico = extractCode(Link, "mixdrop.is/e/");
+            if (CodigoUnico.empty()) {
+                CodigoUnico = extractCode(Link, "mixdrop.is/f/");
             }
-            if (uniqueCode.empty()) {
-                uniqueCode = extractCode(link, "mixdrop.ps/e/");
+            if (CodigoUnico.empty()) {
+                CodigoUnico = extractCode(Link, "mixdrop.ps/e/");
             }
-            if (!uniqueCode.empty()) {
+            if (!CodigoUnico.empty()) {
                 // Sempre retorna no formato "https://mixdrop.ps/e/{codigo}"
-                link = "https://mixdrop.ps/e/" + uniqueCode;
+                Link = "https://mixdrop.ps/e/" + CodigoUnico;
             }
-    } else if (link.find("streamtape.com") != string::npos || link.find("streamtape.to") != string::npos) {
-        // Verifica se o link já está no formato embed "/e/"
-        if (link.find("/e/") == string::npos) {
+
+    // Formata Links streamtape.com (/e/ ou /v/)
+    } else if (Link.find("streamtape.com") != string::npos || Link.find("streamtape.to") != string::npos) {
+        // Verifica se o Link já está no formato embed "/e/"
+        if (Link.find("/e/") == string::npos) {
             // Extrai o código único de "/v/" ou "/e/"
-            uniqueCode = extractCode(link, "streamtape.com/v/");
-            if (uniqueCode.empty()) {
-                uniqueCode = extractCode(link, "streamtape.com/e/");
+            CodigoUnico = extractCode(Link, "streamtape.com/v/");
+            if (CodigoUnico.empty()) {
+                CodigoUnico = extractCode(Link, "streamtape.com/e/");
             }
-            if (uniqueCode.empty()) {
-                uniqueCode = extractCode(link, "streamtape.to/v/");
+            if (CodigoUnico.empty()) {
+                CodigoUnico = extractCode(Link, "streamtape.to/v/");
             }
-            if (!uniqueCode.empty()) {
-                link = "https://streamtape.to/e/" + uniqueCode;
-            }
-        }
-    } else if (link.find("filemoon.sx") != string::npos) {
-        uniqueCode = extractCode(link, "filemoon.sx/e/");
-        if (uniqueCode.empty()) {
-            uniqueCode = extractCode(link, "filemoon.sx/d/");
-            if (uniqueCode.empty()) {
-                uniqueCode = extractCode(link, "filemoon.sx/");
+            if (!CodigoUnico.empty()) {
+                Link = "https://streamtape.to/e/" + CodigoUnico;
             }
         }
-        if (!uniqueCode.empty()) {
-            link = "https://filemoon.sx/e/" + uniqueCode;
+
+    // Formata Links filemoon.sx (/e/ ou /d/)        
+    } else if (Link.find("filemoon.sx") != string::npos) {
+        CodigoUnico = extractCode(Link, "filemoon.sx/e/");
+        if (CodigoUnico.empty()) {
+            CodigoUnico = extractCode(Link, "filemoon.sx/d/");
+            if (CodigoUnico.empty()) {
+                CodigoUnico = extractCode(Link, "filemoon.sx/");
+            }
         }
-    } else if (link.find("yourupload.com") != string::npos) {
-        // Verifica se o link contém "/watch/" e extrai o código
-        uniqueCode = extractCode(link, "/watch/");
+        if (!CodigoUnico.empty()) {
+            Link = "https://filemoon.sx/e/" + CodigoUnico;
+        }
+
+    // Formata Links yourupload.com (/watch/ ou /embed/)
+    } else if (Link.find("yourupload.com") != string::npos) {
+        // Verifica se o Link contém "/watch/" e extrai o código
+        CodigoUnico = extractCode(Link, "/watch/");
 
         // Se não foi encontrado, tenta extrair do formato embed
-        if (uniqueCode.empty()) {
-            uniqueCode = extractCode(link, "/embed/");
+        if (CodigoUnico.empty()) {
+            CodigoUnico = extractCode(Link, "/embed/");
         }
 
-        // Se o código único foi encontrado, forma o link no formato embed
-        if (!uniqueCode.empty()) {
-            link = "https://www.yourupload.com/embed/" + uniqueCode;
+        // Se o código único foi encontrado, forma o Link no formato embed
+        if (!CodigoUnico.empty()) {
+            Link = "https://www.yourupload.com/embed/" + CodigoUnico;
         } else {
-            cout << "Código único não encontrado no link: " << link << endl;
+            cout << "Código único não encontrado no Link: " << Link << endl;
             return ""; // Retorna vazio se nenhum código foi encontrado
         }
-    } else if (link.find("linkbox.to") != string::npos || link.find("lbx.to") != string::npos) {
-        // Extrai o código único do link
-        uniqueCode = extractCode(link, "/f/");
+
+    // Formata Links linkbox.to ou lbx.to (/f/ ou /a/f/)
+    } else if (Link.find("linkbox.to") != string::npos || Link.find("lbx.to") != string::npos) {
+        // Extrai o código único do Link
+        CodigoUnico = extractCode(Link, "/f/");
 
         // Se não encontrou com "f/", tenta extrair com "a/f/"
-        if (uniqueCode.empty()) {
-            uniqueCode = extractCode(link, "a/f/");
+        if (CodigoUnico.empty()) {
+            CodigoUnico = extractCode(Link, "a/f/");
         }
 
-        // Se o código único foi encontrado, forma o link no formato desejado
-        if (!uniqueCode.empty()) {
-            link = "https://www.linkbox.to/a/f/" + uniqueCode;
+        // Se o código único foi encontrado, forma o Link no formato desejado
+        if (!CodigoUnico.empty()) {
+            Link = "https://www.linkbox.to/a/f/" + CodigoUnico;
         } else {
-            cout << "Código único não encontrado no link: " << link << endl;
+            cout << "Código único não encontrado no Link: " << Link << endl;
             return ""; // Retorna vazio se nenhum código foi encontrado
         }
-    } else if (link.find("upstream.to") != string::npos) {
-        uniqueCode = extractCode(link, "embed-");
-        if (uniqueCode.empty()) {
-            uniqueCode = extractCode(link, "upstream.to/");
+    } else if (Link.find("upstream.to") != string::npos) {
+        CodigoUnico = extractCode(Link, "embed-");
+        if (CodigoUnico.empty()) {
+            CodigoUnico = extractCode(Link, "upstream.to/");
         }
 
-        // Remove "embed" do início do uniqueCode, se presente
-        if (uniqueCode.rfind("embed", 0) == 0) { // Verifica se "embed" está no início
-            uniqueCode.erase(0, 5); // Remove os 5 caracteres da palavra "embed"
+        // Remove "embed" do início do CodigoUnico, se presente
+        if (CodigoUnico.rfind("embed", 0) == 0) { // Verifica se "embed" está no início
+            CodigoUnico.erase(0, 5); // Remove os 5 caracteres da palavra "embed"
         }
 
-        if (!uniqueCode.empty()) {
-            link = "https://upstream.to/embed-" + uniqueCode + ".html"; // O link final agora deve estar correto
+        if (!CodigoUnico.empty()) {
+            Link = "https://upstream.to/embed-" + CodigoUnico + ".html"; // O Link final agora deve estar correto
         }
         // Verificar se há múltiplos ".html" no final e remover os excedentes
-        while (link.size() >= 10 && link.substr(link.size() - 10) == ".html.html") {
-            link.erase(link.size() - 5); // Remove o último ".html"
+        while (Link.size() >= 10 && Link.substr(Link.size() - 10) == ".html.html") {
+            Link.erase(Link.size() - 5); // Remove o último ".html"
         }
-    } else if (link.find("streamwish.com") != string::npos) {
+    } else if (Link.find("streamwish.com") != string::npos) {
         // Extrai o código único
-        uniqueCode = extractCode(link, "streamwish.com/d/");
-        if (!uniqueCode.empty()) {
-            link = "https://playerwish.com/e/" + uniqueCode;
+        CodigoUnico = extractCode(Link, "streamwish.com/d/");
+        if (!CodigoUnico.empty()) {
+            Link = "https://playerwish.com/e/" + CodigoUnico;
         }
-    } else if (link.find("playerwish.com") != string::npos) {
-        // O link já está no formato playerwish.com, então só extrai o código para garantir o formato correto
-        uniqueCode = extractCode(link, "playerwish.com/e/");
-        if (!uniqueCode.empty()) {
-            link = "https://playerwish.com/e/" + uniqueCode;
+    } else if (Link.find("playerwish.com") != string::npos) {
+        // O Link já está no formato playerwish.com, então só extrai o código para garantir o formato correto
+        CodigoUnico = extractCode(Link, "playerwish.com/e/");
+        if (!CodigoUnico.empty()) {
+            Link = "https://playerwish.com/e/" + CodigoUnico;
         }
     } else {
-        cout << "Servidor desconhecido ou formato do link inválido." << endl;
+        cout << "Servidor desconhecido ou formato do Link inválido." << endl;
         return "";
     }
 
-    return link; // Retorna o link tratado
+    return Link; // Retorna o Link tratado
 }
 
 // Função para capturar os links das thumbs
 void capturarThumbs() {
-    cout << "Digite os links das Thumbs (precisa informar um link válido para cada episódio):" << endl;
+    cout << "Digite os links das Thumbs (precisa informar um Link válido para cada episódio):" << endl;
     cin.ignore(); // Ignora qualquer caractere de nova linha deixado no buffer pelo cin anterior
 
     int capturados = 0;
@@ -268,7 +298,7 @@ void capturarServidores() {
     servidores.resize(quantidadeServidores);
 
     for (int i = 0; i < quantidadeServidores; ++i) {
-        cout << "Digite os links do servidor " << (i + 1) << " (precisa informar um link válido para cada episódio):" << endl;
+        cout << "Digite os links do servidor " << (i + 1) << " (precisa informar um Link válido para cada episódio):" << endl;
         int capturados = 0;
 
         while (capturados < quantidadeEpisodios) {
